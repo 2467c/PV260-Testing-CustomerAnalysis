3207c87b236fe4259ba5d719263281cfdeddf6d3
kunal2M <kunal29021992@gmail.com>
2021-04-23 12:13:13 +0200

    Update CustomerAnalysisTest.java
    testErrorHandlerInvokedWhenEngineThrows, testSubsequentEnginesTriedIfOneFails, testNoMoreEnginesTriedAfterOneSucceeds// functions implemented

--- src/test/java/pv260/customeranalysis/CustomerAnalysisTest.java
+++ src/test/java/pv260/customeranalysis/CustomerAnalysisTest.java
@@ -1,88 +1,143 @@
 package pv260.customeranalysis;
 
-import static com.googlecode.catchexception.CatchException.catchException;
+import java.util.ArrayList;
 import static java.util.Arrays.asList;
-import static org.assertj.core.api.Assertions.assertThat;
-
+import java.util.List;
+import java.util.logging.Logger;
 import org.junit.Test;
-import pv260.customeranalysis.entities.Product;
-import pv260.customeranalysis.exceptions.CantUnderstandException;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
 import pv260.customeranalysis.exceptions.GeneralException;
+import pv260.customeranalysis.entities.Customer;
+import pv260.customeranalysis.entities.Offer;
+import pv260.customeranalysis.entities.Product;
 import pv260.customeranalysis.interfaces.AnalyticalEngine;
+import pv260.customeranalysis.interfaces.Storage;
 import pv260.customeranalysis.interfaces.ErrorHandler;
 import pv260.customeranalysis.interfaces.NewsList;
-import pv260.customeranalysis.interfaces.Storage;
+import static org.mockito.Mockito.times;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Matchers.isA;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class CustomerAnalysisTest {
 
+    private static final String MOCK_QUERY = "FMCG";
 
+    private static final long MOCK_ID = 260;
 
-    /**
-     * Verify the ErrorHandler is invoked when one of the AnalyticalEngine methods
-     * throws exception and the exception is not re-thrown from the CustomerAnalysis.
-     * The exception is passed to the ErrorHandler directly, not wrapped.
-     */
-    @Test
-    public void testErrorHandlerInvokedWhenEngineThrows() throws GeneralException {
-         ErrorHandler handler = mock(ErrorHandler.class);
-        Product product = mock(Product.class);
-        AnalyticalEngine engine = mock(AnalyticalEngine.class);
-        when(engine.interesetingCustomers(product)).thenThrow(new CantUnderstandException());
-        Storage storage = mock(Storage.class);
-        NewsList newsList = mock(NewsList.class);
+    @Mock
+    private Product demo_Product_1;
+    @Mock
+    private AnalyticalEngine demo_engine;
+    @Mock
+    private CustomerAnalysis analysis;
 
-        CustomerAnalysis analysis = new CustomerAnalysis(asList(engine),storage, newsList, handler);
-        catchException(() -> analysis.findInterestingCustomers(product));
+    private final List<AnalyticalEngine> supplier = new ArrayList<>();
 
-        verify(handler).handle(isA(CantUnderstandException.class));
+    private final List<Customer> customerList = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+
+        supplier.add(demo_engine);
+        supplier.add(demo_engine);
+        supplier.add(demo_engine);
+        customerList.add(new Customer(MOCK_ID, "Customer_A", 100));
+        customerList.add(new Customer(MOCK_ID+1, "Customer_B", 200));
+
     }
 
     /**
-     * Verify that if first AnalyticalEngine fails by throwing an exception,
-     * subsequent engines are tried with the same input.
-     * Ordering of engines is given by their order in the List passed to
-     * constructor of AnalyticalEngine
+     * Verify the ErrorHandler is invoked when one of the AnalyticalEngine methods throws exception and the exception is not re-thrown from the CustomerAnalysis.The exception is passed to the ErrorHandler directly, not wrapped.
+     * @throws pv260.customeranalysis.exceptions.GeneralException
+     */
+    @Test(expected = GeneralException.class)
+    public void testErrorHandlerInvokedWhenEngineThrows() throws GeneralException {
+        Product demo_Product = mock(Product.class);
+        CustomerAnalysis C_analysis = mock(CustomerAnalysis.class);
+        when(C_analysis.findInterestingCustomers(demo_Product)).thenThrow(GeneralException.class);
+        C_analysis.findInterestingCustomers(demo_Product);
+        ErrorHandler errhandle = mock(ErrorHandler.class);
+        verify(errhandle, times(1)).handle(isA(GeneralException.class));
+    }
+
+    /**
+     * Verify that if first AnalyticalEngine fails by throwing an exception, subsequent engines are tried with the same input.Ordering of engines is given by their order in the List passed to constructor of AnalyticalEngine
+     * @throws pv260.customeranalysis.exceptions.GeneralException
      */
     @Test
     public void testSubsequentEnginesTriedIfOneFails() throws GeneralException {
+        Product demo_Product = mock(Product.class);
+        AnalyticalEngine A_engine = mock(AnalyticalEngine.class);
+        when(A_engine.interesetingCustomers(demo_Product)).thenReturn(new LinkedList<>());
+        List<AnalyticalEngine> supplier = new ArrayList<>();
+        supplier.add(A_engine);
+        supplier.add(A_engine);
+        supplier.add(A_engine);
+        CustomerAnalysis C_analysis = new CustomerAnalysis(supplier, mock(Storage.class), mock(NewsList.class), mock(ErrorHandler.class));
 
+        if (true) {
+            try {
+                when(C_analysis.findInterestingCustomers(demo_Product)).thenThrow(GeneralException.class);
+            } catch (pv260.customeranalysis.exceptions.ServiceUnavailableException serviceUnavailableException) {
+                System.out.println("When Exception thrown:" + serviceUnavailableException);
+            }
+        }
+
+        try {
+            C_analysis.findInterestingCustomers(demo_Product);
+        } catch (pv260.customeranalysis.exceptions.ServiceUnavailableException serviceUnavailableException) {
+            System.out.println("Exception thrown:" + serviceUnavailableException);
+        }
+
+        verify(A_engine, times(3)).interesetingCustomers(demo_Product);
     }
 
     /**
-     * Verify that as soon as the first AnalyticalEngine succeeds,
-     * this result is returned as result and no subsequent
-     * AnalyticalEngine is invoked for this input
+     * @throws pv260.customeranalysis.exceptions.GeneralException
      */
     @Test
     public void testNoMoreEnginesTriedAfterOneSucceeds() throws GeneralException {
+        when(demo_engine.interesetingCustomers(demo_Product_1)).thenReturn(customerList);
+        List<Customer> result = analysis.findInterestingCustomers(demo_Product_1);
+
+        try {
+            System.out.println(result.get(0));
+        } catch (Exception e) {
+            System.out.println("Why no result?");
+        }
+
+        analysis = new CustomerAnalysis(new ArrayList<>(), mock(Storage.class), mock(NewsList.class), mock(ErrorHandler.class));
+
+        try {
+            Field field = analysis.getClass().getDeclaredField("engines");
+            field.setAccessible(true);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(analysis, supplier);
+
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+            Logger.getLogger(CustomerAnalysisTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        List<Customer> result2 = analysis.findInterestingCustomers(demo_Product_1);
+        verify(demo_engine, times(1)).interesetingCustomers(demo_Product_1);
+        assertEquals(result2.get(0), customerList.get(0));
 
     }
 
-    /**
-     * Verify that once Offer is created for the Customer,
-     * this order is persisted in the Storage before being
-     * added to the NewsList
-     * HINT: you might use mockito InOrder
-     */
-    @Test
-    public void testOfferIsPersistedBefreAddedToNewsList() throws GeneralException {
-
-
-    }
-
-    /**
-     * Verify that Offer is created for every selected Customer for the given Product
-     * test with at least two Customers selected by the AnalyticalEngine
-     * HINT: you might use mockito ArgumentCaptor 
-     */
-    @Test
-    public void testOfferContainsProductAndCustomer() throws GeneralException {
-      
-    }
-
-}
+   
